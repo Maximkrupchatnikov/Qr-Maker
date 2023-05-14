0381d4aefa5c027fdd44814182c59ab26afec764
Maximkrupchatnikov <88796362+Maximkrupchatnikov@users.noreply.github.com>
2023-05-14 15:19:30 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,91 +8,127 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
+// функция для создания qr кода, принимает переменную S, имя файля для сохранения
 func qr(s string, filename string) {
-
 	url := "http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl="
-	url += s
+	url = url + s
 	resp, err := http.Get(url)
-
 	if err != nil {
 		panic(err)
 	}
-
-	img, _, err := image.Decode(resp.Body) // декдироание полченного отеа на зпрос get запрос и запись в переменную img
+	// декдироание полченного отеа на зпрос get запрос и запись в переменную img
+	img, _, err := image.Decode(resp.Body)
 	if err != nil {
 		panic(err)
 	}
+	//создание файла с qr кодом
 	if filename == "0" {
 		file, err := os.Create("qr_code" + s + ".png")
 		if err != nil {
-			panic(err)
+			fmt.Println("Ошибка при созранении qr кода", err)
+			return
 		}
 		png.Encode(file, img)
+		defer file.Close()
 	} else {
-		file, err := os.Create(filename + "qr_code" + s + ".png")
+		file, err := os.Create(filename + "/" + "qr_code" + s + ".png")
 		if err != nil {
-			panic(err)
+			fmt.Println("Ошибка сохранения qr кода", err)
+			return
 		}
 		png.Encode(file, img)
+		defer file.Close()
 	}
-	
-	if err != nil {
-		panic(err)
-	}
-	if err != nil {
-		panic(err)
-	}
-
 }
 
-func keyb() {
+func keyB() {
 	fmt.Print("Введите кол-во запросов:", " ")
-
+	//считывание кол-во запросов
 	var n int
 	fmt.Scanln(&n)
 
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("Введите имя папки для сохранения изображения: ")
+	fmt.Print("Введите имя папки для сохранения изображения: \n")
 	scanner.Scan()
 	filename := scanner.Text()
 
-	fmt.Print("Введите текст для преобразования в qr код:")
+	fmt.Print("Введите текст для преобразования в qr код: \n")
 
 	for i := 0; i < n; i++ {
-		sc := bufio.NewScanner(os.Stdin)
-		sc.Scan()
-		s := sc.Text()
+		scanner = bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		s := scanner.Text()
 		s = strings.Replace(s, " ", "+", -1)
 		qr(s, filename)
 	}
+	fmt.Println("Программа завершила свое выполнение ")
+	main()
 }
 
 func textfile() {
+	fmt.Println("Введите имя папки c текстом для преобразования в qr код: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	file := scanner.Text()
 
-}
+	var filename string
+	fmt.Println("Введите имя папки для сохоранеения туда qr кодов")
+	fmt.Scan(&filename)
+	start := time.Now()
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Ошибка сканирования папки")
+		return
+	}
 
-func JsonEnCode() {
+	files, err := os.Open(file)
+	if err != nil {
+		fmt.Println("Ошибка открытия файла", err)
+		return
+	}
 
+	scanner = bufio.NewScanner(files)
+	scanner.Split(bufio.ScanWords)
+	var wg sync.WaitGroup
+	for scanner.Scan() {
+		word := scanner.Text()
+		word = strings.TrimSpace(word)
+
+		wg.Add(1)
+		go func(word, filename string) {
+			defer wg.Done()
+			qr(word, filename)
+		}(word, filename)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Ошибка считывания файла", err)
+		return
+	}
+	wg.Wait()
+	fmt.Println("Программа завершила свое выполнение за", time.Since(start))
+	main()
 }
 
 func main() {
 	var value string
+	// Выводим пользвательский интерфейс
 	fmt.Println("Выберите действие:")
-	fmt.Println("1. Ввод с терминала")
+	fmt.Println("1. Ввод c терминала")
 	fmt.Println("2. Считывание текстового файла")
-	fmt.Println("3. Считывание JSON файла")
+	fmt.Println("3. Выйти из программы")
 
 	fmt.Scanln(&value)
-
+	// стек вызова функций
 	switch value {
 	case "1":
-		keyb()
+		keyB()
 	case "2":
 		textfile()
-	case "3":
-		JsonEnCode()
+	case "4":
+		break
 	default:
 		fmt.Println("читай еще раз")
 	}
